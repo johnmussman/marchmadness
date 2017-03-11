3bfc8370d4bf0f3ccc3fb11710af14d1b01897ab
John <jm@johnmussman.com>
2017-03-10 23:20:20 -0500

    tested homecourt advantage

--- mle.py
+++ mle.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from lloss import logloss
 from random import sample
 import time
+from math import sqrt
 
 
 
@@ -13,6 +14,43 @@ PATIENCE = 1000
 def win_likelihood(p1, p2):
 	return (p1 * (1 - p2) /
 		(p1 * (1 - p2) + p2 * (1 - p1)))
+
+# def load_team_geo():
+# 	team_geo = {}
+# 	with open('../madness/input/TeamGeog.csv', 'r') \
+# 			as fp:
+# 		next(fp)
+# 		for line in fp:
+# 			line = line.strip()
+# 			team, lt, lg = line.split(',')
+# 			team_geo[team] = (float(lt), float(lg))
+# 	return team_geo
+
+# # def load_tourney_geo():
+# # 	tourney_geo = {}
+# # 	with open('../madness/input/TourneyGeog.csv', 'r') \
+# # 			as fp:
+# # 		next(fp)
+# # 		for line in fp:
+# # 			line = line.strip()
+# # 			year, day, wteam, lteam, slot, host, lat, lng = line.split(',')
+# # 			tourney_geo[(year, day, wteam, lteam)] = (float(lat), float(lng))
+# # 	return tourney_geo
+
+# # def assess_homecourt(wcoords, lcoords, tcoords):
+# # 	wdist = sqrt((wcoords[0] - tcoords[0])**2 + (wcoords[1] - tcoords[1])**2)
+# # 	ldist = sqrt((lcoords[0] - tcoords[0])**2 + (lcoords[1] - tcoords[1])**2)
+# # 	return (wdist - ldist) / (wdist + ldist)
+
+# # def assess_homecourt_lat(wcoords, lcoords, tcoords):
+# # 	wdist = sqrt((wcoords[0] - tcoords[0])**2)
+# # 	ldist = sqrt((lcoords[0] - tcoords[0])**2)
+# # 	return (wdist - ldist) / (wdist + ldist)
+
+# # def assess_homecourt_lng(wcoords, lcoords, tcoords):
+# # 	wdist = sqrt((wcoords[1] - tcoords[1])**2)
+# # 	ldist = sqrt((lcoords[1] - tcoords[1])**2)
+# # 	return (wdist - ldist) / (wdist + ldist)
 
 # various_ratings = []
 # TEST = 10
@@ -47,7 +85,6 @@ with open('../madness/input/RegularSeasonCompactResults.csv') \
 			wins_for_team[wteam] += 1
 			losses_for_team[lteam] += 1
 
-# (winner, loser)
 tourney_outcomes = []
 with open('../madness/input/TourneyCompactResults.csv') \
 		as fp:
@@ -55,20 +92,17 @@ with open('../madness/input/TourneyCompactResults.csv') \
 	for line in fp:
 		year, day, wteam, wscore, lteam, lscore, wloc, numot = line.strip().split(',')
 		if year == YEAR:
-			tourney_outcomes.append((wteam, lteam))
+			tourney_outcomes.append((year, day, wteam, lteam))
 
 ratings = {}
-
 for team in game_indices_by_team:
 	ratings[team] = float(wins_for_team[team]) / (wins_for_team[team] + losses_for_team[team])
 
-# loop to fit MLEs
 start = time.time()
 actual = [1] * len(games)
 prediction = []
 for wteam, lteam in games:
 	prediction.append(win_likelihood(ratings[wteam], ratings[lteam]))
-
 curr_ll, best_ll = 10, 10
 convergence = 0
 while convergence < PATIENCE:
@@ -76,7 +110,7 @@ while convergence < PATIENCE:
 	provisional_rating = ratings[team] + (sample([1, -1], 1)[0] * EPSILON)
 	provisional_prediction = prediction[:]
 	for game_idx in game_indices_by_team[team]:
-		if team == games[game_idx][0]:	
+		if team == games[game_idx][0]:
 			provisional_prediction[game_idx] = win_likelihood(provisional_rating, ratings[games[game_idx][1]])
 		else:
 			provisional_prediction[game_idx] = win_likelihood(ratings[games[game_idx][0]], provisional_rating)
@@ -89,17 +123,23 @@ while convergence < PATIENCE:
 		for game_idx in game_indices_by_team[team]:
 			prediction[game_idx] = provisional_prediction[game_idx]
 		convergence = 0
+print time.time() - start, " seconds to converge."
 
-# add home-game advantage! say it improves win likelihood by N percent
-
-# validation
+# team_geo = load_team_geo()
+# tourney_geo = load_tourney_geo()
+# for i in range(10):
+# 	for j in range(10):
+# 		gi = float(i) / 1000
+# 		gj = float(j) / 1000
 actual = [1] * len(tourney_outcomes)
 predicted = []
-for wteam, lteam in tourney_outcomes:
+for year, day, wteam, lteam in tourney_outcomes:
+			# lat_margin = assess_homecourt_lat(team_geo[wteam], team_geo[lteam], tourney_geo[(year, day, wteam, lteam)]) * gi
+			# lng_margin = assess_homecourt_lng(team_geo[wteam], team_geo[lteam], tourney_geo[(year, day, wteam, lteam)]) * gj
+			#  + lat_margin * gi + lng_margin * gj
 	predicted.append(win_likelihood(ratings[wteam], ratings[lteam]))
 curr_ll = logloss(actual, predicted)
-print YEAR, 'score: ', curr_ll
-print time.time() - start, " seconds to converge."
+print YEAR, gi, gj, 'score: ', curr_ll
 
 # 	various_ratings.append(ratings)
 
